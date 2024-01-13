bafa285e9ef5a6521fabb2cba7b0423d29f6cabc
Ilias Haddad <42832768+IliasHad@users.noreply.github.com>
2024-01-13 23:29:39 +0100

    initial commit

---
+++ src/app/work/page.jsx
@@ -0,0 +1,88 @@
+import Link from 'next/link'
+
+import { Border } from '@/components/Border'
+import { Button } from '@/components/Button'
+import { ContactSection } from '@/components/ContactSection'
+import { Container } from '@/components/Container'
+import { FadeIn } from '@/components/FadeIn'
+import { PageIntro } from '@/components/PageIntro'
+import { formatDate } from '@/lib/formatDate'
+import { loadProjects } from '@/lib/mdx'
+import { Footer } from '@/components/Footer'
+
+function Projects({ projects }) {
+  return (
+    <Container className="mt-40">
+      <FadeIn>
+        <h2 className="font-display text-2xl font-semibold text-neutral-950">
+          Projects
+        </h2>
+      </FadeIn>
+      <div className="mt-10 space-y-20 sm:space-y-24 lg:space-y-32">
+        {projects.map((project) => (
+          <FadeIn key={project.href}>
+            <article>
+              <Border className="grid grid-cols-3 gap-x-8 gap-y-8 pt-16">
+                <div className="col-span-full sm:flex sm:items-center sm:justify-between sm:gap-x-8 lg:col-span-1 lg:block">
+                  <div className="mt-1 flex gap-x-4 sm:mt-0 lg:block">
+                    <p className="text-sm tracking-tight text-neutral-950 after:ml-4 after:font-semibold after:text-neutral-300 after:content-['/'] lg:mt-2 lg:after:hidden">
+                      {project.service}
+                    </p>
+                    <p className="text-sm text-neutral-950 lg:mt-2">
+                      <time dateTime={project.date}>
+                        {formatDate(project.date)}
+                      </time>
+                    </p>
+                  </div>
+                </div>
+                <div className="col-span-full lg:col-span-2 lg:max-w-2xl">
+                  <p className="font-display text-4xl font-medium text-neutral-950">
+                    <Link href={project.href}>{project.title}</Link>
+                  </p>
+
+                  <div className="mt-8 flex">
+                    <Button
+                      href={project.href}
+                      aria-label={`Read project: ${project.client}`}
+                    >
+                      Read project
+                    </Button>
+                  </div>
+                </div>
+              </Border>
+            </article>
+          </FadeIn>
+        ))}
+      </div>
+    </Container>
+  )
+}
+
+export const metadata = {
+  title: 'Our Work',
+  description:
+    'We believe in efficiency and maximizing our resources to provide the best value to our clients.',
+}
+
+export default async function Work() {
+  let projects = await loadProjects()
+
+  return (
+    <>
+      <PageIntro
+        eyebrow="Our work"
+        title="Proven solutions for real-world problems."
+      >
+        <p>
+          We believe in efficiency and maximizing our resources to provide the
+          best value to our clients. The primary way we do that is by re-using
+          the same five projects we’ve been developing for the past decade.
+        </p>
+      </PageIntro>
+
+      <Projects projects={projects} />
+
+      <Footer />
+    </>
+  )
+}
